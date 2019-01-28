208bd7d72596172959e830b79df35b08c260386b
gohanmystic <vutran26999@gmail.com>
2019-01-28 20:19:10 +0700

    update auto increment pk

--- src/main/java/vn/com/fsoft/model/HocSinh.java
+++ src/main/java/vn/com/fsoft/model/HocSinh.java
@@ -11,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
@@ -24,7 +25,8 @@ import lombok.Setter;
 public class HocSinh {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_seq_gen")
+	@SequenceGenerator(name = "users_seq_gen", sequenceName = "users_id_seq")
 	@Column(name = "Ma_Hoc_Sinh", length = 5)
 	private Integer maHocSinh;
 
